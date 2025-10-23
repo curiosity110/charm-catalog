1143e1178ac1a9403eaf13511c508fc6f43827ed
curiosity110 <91900298+curiosity110@users.noreply.github.com>
2025-10-23 18:51:46 +0200

    Replace cart with quick order dialog flow

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { CartProvider } from "./contexts/CartContext";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <CartProvider>
-    <App />
-  </CartProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
